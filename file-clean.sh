872fc124ffe595c47a5dfce9a153487b53489166
leeroywking <leeroywking@gmail.com>
2019-08-05 14:43:58 -0700

    got it

--- file-clean.sh
+++ file-clean.sh
@@ -1,8 +1,8 @@
 while read file
 do
 echo "removing rando bytes from  $file"
-tr '\r' '\n' < /paste/$file > /paste/cleaned/$file
-tr -cd '[:alnun:\n\:]'< /paste/cleaned/$file > /paste/cleaned/cleaner/$file
+# tr '\r' '\n' < /paste/$file > /paste/cleaned/$file
+tr -cd '[:print:]'< /paste/$file > /paste/cleaned/$file
 echo "done removing null bystes from $file"
 echo "validating with dos2unix"
 dos2unix /paste/cleaned/cleaner/$file
