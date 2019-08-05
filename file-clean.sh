c74a4ecf514862f9fa3845909e1c394e918434a6
leeroywking <leeroywking@gmail.com>
2019-08-05 14:30:49 -0700

    more logging and dos2unix

--- file-clean.sh
+++ file-clean.sh
@@ -3,4 +3,7 @@ do
 echo "removing rando bytes from  $file"
 tr -cd '[:alnum:]'< /paste/$file > /paste/cleaned/$file
 echo "done removing null bystes from $file"
+echo "validating with dos2unix"
+dos2unix /paste/cleaned/$file
+echo "done validating"
 done < list.txt                                               
