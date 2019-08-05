896a6b81b945e4d01bd65e92fe0e141abbcdabf1
leeroywking <leeroywking@gmail.com>
2019-08-05 14:51:19 -0700

    messing

--- file-clean.sh
+++ file-clean.sh
@@ -2,7 +2,7 @@ while read file
 do
 echo "removing rando bytes from  $file"
 # tr '\r' '\n' < /paste/$file > /paste/cleaned/$file
-tr -cd '[:print:]'< /paste/$file > /paste/cleaned/$file
+tr -cd '[:print:]\r\n'< /paste/$file > /paste/cleaned/$file
 echo "done removing null bystes from $file"
 echo "validating with dos2unix"
 dos2unix /paste/cleaned/$file
