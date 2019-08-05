7bf22b07857351fbafa0e18a1d6bf2b20baa3df1
leeroywking <leeroywking@gmail.com>
2019-08-05 14:13:19 -0700

    log text

--- file-clean.sh
+++ file-clean.sh
@@ -1,4 +1,6 @@
 while read file
 do
+echo "removing null bytes from  $file"
 tr < /paste/$file -d '\000' > /paste/cleaned/$file
+echo "done removing null bystes from $file"
 done < list.txt                                                
