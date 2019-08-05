52f49930086355bd70bcd71e46a2f3ac373ff832
leeroywking <leeroywking@gmail.com>
2019-08-05 14:28:06 -0700

    alphanum

--- file-clean.sh
+++ file-clean.sh
@@ -1,6 +1,6 @@
 while read file
 do
 echo "removing rando bytes from  $file"
-tr -cd '\11\13\14\15\40-\176'< /paste/$file > /paste/cleaned/$file
+tr -cd '[:alnum:]'< /paste/$file > /paste/cleaned/$file
 echo "done removing null bystes from $file"
 done < list.txt                                               
