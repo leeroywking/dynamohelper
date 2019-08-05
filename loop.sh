2fa130bdbfaa84b1c11d3ed50d039fcea5b140b3
leeroywking <leeroywking@gmail.com>
2019-08-05 16:39:11 -0700

    disaster looms

--- loop.sh
+++ loop.sh
@@ -3,15 +3,15 @@
 while read line
 do
 	IFS=':' read -ra array <<< "$line";
-	a='{"user": {"S": "'
+	a='{"PutRequest": {"Item": {"user": {"S": "'
 	b=${array[0]}
 	c='"},"pass": {"S": "'
 	d=${array[1]}
-	e='"}}'
+	e='"}}}},'
 	f="$a$b$c$d$e"
-	echo "'$f'"
-		aws dynamodb put-item \
-		--table-name usepa \
-		--item "$f"
+	echo "$f" >> $2.json
+		# aws dynamodb put-item \
+		# --table-name usepa \
+		# --item "$f" &
 done < $1
 
--- runloop.sh
+++ runloop.sh
@@ -2,5 +2,11 @@
 
 while read file 
 do
-bash loop.sh /paste/cleaned/"$file" &
+echo $file
+echo '{"usepa": [' >> $file.json
+bash loop.sh $file $file
+# bash loop.sh /paste/cleaned/"$file" $file
+sed '$ s/,$//g' $file.json > $file.next.json
+echo ']}' >> $file.next.json
+aws dynamodb batch-write-item --request-items file://$file.next.json
 done < list.txt
